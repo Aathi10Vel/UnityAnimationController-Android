188e485d2ae750988409d9234421eeff2baa1042
Aathi <37983032+Aathi10Vel@users.noreply.github.com>
2018-06-13 19:07:22 +0530

    Add files via upload

---
+++ FollowCamera.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Transform targetPlayer;
+	Camera playerCamera;
+	public PlayerMove getActions;
+	public float cameraDistance=3.0f;
+	public float cameraSensitivity = 3.0f;
+	public float camSmooth=2.0f;
+
+	public float charSmooth=3.0f;
+	public float smoothVel=10.0f;
+
+	public Vector2 mouseLook;
+	public Vector2 rotateVert;
+
+	public Quaternion camRotateX;
+	public Quaternion camRotateXY;
+
+	public Vector3 lookOffset;
+	public Vector3 camPosition;
+
+
+
+	// Use this for initialization
+	void Start () {
+		getActions = FindObjectOfType<PlayerMove>();
+		playerCamera = GetComponent<Camera> ();
+		lookOffset = playerCamera.transform.position - targetPlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camControl ();	
+	}
+
+	public void camControl(){
+		var mouseDir = new Vector2 (CrossPlatformInputManager.GetAxisRaw ("Mouse X"), CrossPlatformInputManager.GetAxisRaw ("Mouse Y"));
+		mouseDir = Vector2.Scale (mouseDir, new Vector2 (cameraSensitivity* camSmooth, cameraSensitivity* camSmooth));
+		rotateVert.x = Mathf.Lerp (rotateVert.x, mouseDir.x, 1f / camSmooth);
+		rotateVert.y = Mathf.Lerp (rotateVert.y, mouseDir.y, 1f / camSmooth);
+		mouseLook += rotateVert;
+		mouseLook.y = Mathf.Clamp (mouseLook.y, -40, 40);
+
+		camRotateX = Quaternion.Euler (0, mouseLook.x, 0);
+		camRotateXY = Quaternion.Euler (-mouseLook.y, mouseLook.x, 0);
+
+		if (getActions.actionPerform == true) {
+			// This can smooth the Player Turn
+			targetPlayer.eulerAngles = Vector3.up * Mathf.SmoothDampAngle (targetPlayer.transform.eulerAngles.y, camRotateX.eulerAngles.y, ref smoothVel, charSmooth * Time.deltaTime);
+		} else {
+			Vector3 lookPoint = targetPlayer.transform.position;
+			playerCamera.transform.LookAt (lookPoint + lookOffset);
+		}
+
+		camPosition=targetPlayer.position-(camRotateXY*Vector3.forward*cameraDistance+new Vector3(0,-lookOffset.y,0));
+
+		playerCamera.transform.rotation = camRotateXY;
+		playerCamera.transform.position = camPosition;
+
+	}
+}
---
+++ PlayerMove.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour {
+    public Animator animBrute;
+    public CharacterController bruteControl;
+    public Vector3 bruteMovement = Vector3.zero;
+
+    public float verticalMove;
+    public float horizontalMove;
+    public float spriteSpeed;
+
+    public float playerSpeed=5.0f;
+    public float playerGravity = 20.0f;
+    public float playerVelocity = 0.0f;
+    public float playerJumpSpeed = 1.0f;
+    public bool actionPerform;
+
+    public float getSpeed;
+    public float getDirection;
+
+    public bool spriteToggle;
+    public Button spriteButton;
+    public Button jumpButton;
+    public Sprite charRun, charWalk;
+
+    // Use this for initialization
+    void Start () {
+        spriteToggle = false;
+        actionPerform = false;
+        animBrute = GetComponent<Animator>();
+        bruteControl = GetComponent<CharacterController>();
+        spriteButton.image.overrideSprite = charRun;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerMovement();
+        playerAnimationController();
+	}
+
+    public void playerAnimationController() {
+        getSpeed = animBrute.GetFloat("Speed");
+        getDirection = animBrute.GetFloat("Direction");
+
+        verticalMove = CrossPlatformInputManager.GetAxis("Vertical");
+        horizontalMove = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        spriteButton.onClick.AddListener(spriteRunning);
+
+        if (spriteToggle)
+        {
+            if (getSpeed != 0 || getDirection != 0)
+            {
+                spriteSpeed = 0.2f;
+                playerSpeed = 10.0f;
+            }
+            else {
+                spriteSpeed = 0.0f;
+                playerSpeed = 5.0f;
+            }
+        }
+
+        if(getSpeed ==0 && getDirection == 0)
+        {
+            spriteSpeed = 0.0f;
+            actionPerform = false;
+        }
+    }
+
+    public void playerMovement() {
+        bruteMovement = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical"));
+        bruteMovement = transform.TransformDirection(bruteMovement);
+        playerVelocity -= playerGravity * Time.deltaTime;
+        bruteMovement.y = playerVelocity;
+        bruteMovement *= playerSpeed;
+        bruteControl.Move(bruteMovement * Time.deltaTime);
+        actionPerform = true;
+
+        jumpButton.onClick.AddListener(bruteJump);
+    }
+
+    public void LateUpdate()
+    {
+        animBrute.SetFloat("Speed", verticalMove);
+        animBrute.SetFloat("Direction", horizontalMove);
+        animBrute.SetFloat("Sprite",spriteSpeed);
+        animBrute.SetFloat("JumpSpeed", playerVelocity);
+    }
+
+    public void spriteRunning() {
+        if (!spriteToggle)
+        {
+            spriteToggle = true;
+            spriteButton.image.overrideSprite = charWalk;
+        }
+        else
+        {
+            spriteToggle = false;
+            spriteButton.image.overrideSprite = charRun;
+        }
+    }
+
+    public void bruteJump()
+    {
+        if (bruteControl.isGrounded)
+        {
+            playerVelocity = playerJumpSpeed;
+        }
+    }
+}
